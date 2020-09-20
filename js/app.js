e08d9ddc5bf9eedff2154b6f3643f0085523c60e
Mary Whistler <marywhistler@me.com>
2020-09-19 20:05:07 -0400

    add

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+
+data.forEach(function("ufo-table") {
+   console.log("ufo-table";
+ });
+
+
+data.forEach(function("ufo-table") {
+    console.log("ufo-table");
+        var row = tbody.append("tr");
+});
+
+var button = d3.select("filter-btn");
+    button.on("click", function(datetime) {
+        var date = d3.select("datetime").text
+
+    }
+if (key === "datetime") {
+      data.push("tr");
+    }
+    else {
+      data.push();
+    }
+   });
+});
